3c834e5e0e492e924212caf34d4a2b09faffb421
guleixibian <guleixibian@outlook.com>
2022-02-23 20:19:12 +0800

    Finally the music comes!

--- assets/JavaScript/BackgroundMusic.js
+++ assets/JavaScript/BackgroundMusic.js
@@ -5,14 +5,15 @@ $(document).ready(
             function(){
                 $("#Music_Start").hide(300);
                 $("#Music_Stop").show(300);
-                $("audio #BGM").css({"autoplay" : "autoplay", "loop" : "loop"});
+                // Both " and / must be escaped
+                $("#Music_Start").after(" <audio id=\"BGM\" autoplay=\"autoplay\" src=\"..\/Hand-Written-HTML-Site\/assets\/Musics\/Move.mp3\"></audio>");
             }
         );
         $("#Music_Stop").click(
             function(){
                 $("#Music_Start").show(300);
                 $("#Music_Stop").hide(300);
-                $("audio #BGM").css({"autoplay" : "false", "loop" : "false"});
+                $("#BGM").remove();
             }
         );
     }
