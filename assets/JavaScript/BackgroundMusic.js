cdcf0ee9ad6cbe67ac68e4b9674c4225216acdec
guleixibian <guleixibian@outlook.com>
2022-02-23 18:12:04 +0800

    Add an animation time for the music buttons

--- assets/JavaScript/BackgroundMusic.js
+++ assets/JavaScript/BackgroundMusic.js
@@ -3,16 +3,17 @@ $(document).ready(
         $("#Music_Stop").hide();
         $("#Music_Start").click(
             function(){
-                $("#Music_Start").hide();
-                $("#Music_Stop").show();
-                $("#Music_Start").append(" <audio src=\"../assets/Musics/Move.mp3\"  autoplay=\"autoplay\"> </audio> ");
+                $("#Music_Start").hide(300);
+                $("#Music_Stop").show(300);
+                $("audio #BGM").css({"autoplay" : "autoplay", "loop" : "loop"});
             }
         );
         $("#Music_Stop").click(
             function(){
-                $("#Music_Start").show();
-                $("#Music_Stop").hide();
+                $("#Music_Start").show(300);
+                $("#Music_Stop").hide(300);
+                $("audio #BGM").css({"autoplay" : "false", "loop" : "false"});
             }
-        )
+        );
     }
 );
