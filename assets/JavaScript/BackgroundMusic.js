17d91d8b4718cbba3e30fe4e58e29f1e3f2f81e8
guleixibian <guleixibian@outlook.com>
2022-02-25 08:45:12 +0800

    Optimize the music button

--- assets/JavaScript/BackgroundMusic.js
+++ assets/JavaScript/BackgroundMusic.js
@@ -1,20 +1,15 @@
-$(document).ready(
-    function(){
-        $("#Music_Stop").hide();
-        $("#Music_Start").click(
-            function(){
-                $("#Music_Start").hide(300);
-                $("#Music_Stop").show(300);
-                // Both " and / must be escaped
-                $("#Music_Start").after(" <audio id=\"BGM\" autoplay=\"autoplay\" src=\"..\/Hand-Written-HTML-Site\/assets\/Musics\/Move.mp3\"></audio>");
-            }
-        );
-        $("#Music_Stop").click(
-            function(){
-                $("#Music_Start").show(300);
-                $("#Music_Stop").hide(300);
-                $("#BGM").remove();
-            }
-        );
-    }
-);
+$(document).ready(function(){
+    $("#Music_Stop").hide();
+    $("#Music_Start").click(function(){
+        $("#Music_Start").hide(300);
+        $("#Music_Stop").show(300);
+        // Both " and / must be escaped
+        $("#Music_Start").after(" <audio id=\"BGM\" autoplay=\"autoplay\" src=\"..\/Hand-Written-HTML-Site\/assets\/Musics\/Move.mp3\"></audio>");
+    });
+
+    $("#Music_Stop").click(function(){
+        $("#Music_Start").show(300);
+        $("#Music_Stop").hide(300);
+        $("#BGM").remove();
+    });
+});
