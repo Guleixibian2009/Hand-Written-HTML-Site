a17e35a9d051f5945581f38ccd059ca57d263192
guleixibian <guleixibian@outlook.com>
2022-02-28 16:44:28 +0800

    The Copy_Button starts working & import ClipboardJS

---
+++ assets/JavaScript/CopyCodeblock.js
@@ -0,0 +1,24 @@
+"use strict";
+$(document).ready(function(){
+    $(".Copy_Button").click(function(){
+        var clipboard = new ClipboardJS(".Copy_Button",{
+            target:(trigger) => trigger.previousElementSibling,
+        });
+
+        clipboard.on('success',(e)=>{
+            console.log("Successfully copied to clipboard!");
+
+            e.clearSelection();
+        });
+          
+        clipboard.on('error',()=>{
+            console.error("Copy Failed!");
+        });
+
+        const Button = $(".Copy_Button");
+        Button.html("<i class=\"fa fa-check fa-fw\"></i>");
+        window.setTimeout(function(){
+            Button.html("<i class=\"fa fa-clipboard fa-fw\"></i>");
+        },1000);
+    });
+});
