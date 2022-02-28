c595768d3d04cfbee52d5e66695e608c5bebb537
guleixibian <guleixibian@outlook.com>
2022-02-28 09:27:54 +0800

    Upgrade the music button into 1 button

--- assets/JavaScript/BackgroundMusic.js
+++ assets/JavaScript/BackgroundMusic.js
@@ -1,15 +1,37 @@
+"use strict";
 $(document).ready(function(){
-    $("#Music_Stop").hide();
-    $("#Music_Start").click(function(){
-        $("#Music_Start").hide(300);
-        $("#Music_Stop").show(300);
-        // Both " and / must be escaped
-        $("#Music_Start").after(" <audio id=\"BGM\" autoplay=\"autoplay\" src=\"..\/Hand-Written-HTML-Site\/assets\/Musics\/Move.mp3\"></audio>");
-    });
+    const Music_Button = $("#Music");
+    var status = 0;
+    // Not Playing = 0; Playing = 1;
+    document.getElementById("Music").innerHTML = " <i class=\"fas fa-play\"></i> Music!";
 
-    $("#Music_Stop").click(function(){
-        $("#Music_Start").show(300);
-        $("#Music_Stop").hide(300);
+    function Start(){
+        status = 1;
+        $(Music_Button).slideToggle(200);
+        window.setTimeout(function(){
+            document.getElementById("Music").innerHTML = "\<i class=\"fas fa-pause\"\>\</i\> Axero/Itro--Move";
+        }, 200);
+        // Both " and / must be escaped!
+        $(Music_Button).after(" <audio id=\"BGM\" autoplay=\"autoplay\" src=\"..\/Hand-Written-HTML-Site\/assets\/Musics\/Move.mp3\"></audio>");
+        $(Music_Button).slideToggle(200);
+    }
+
+    function Stop(){
+        status = 0;
+        $(Music_Button).slideToggle(200);
+        window.setTimeout(function(){
+            document.getElementById("Music").innerHTML = " <i class=\"fas fa-play\"></i> Music!";
+        }, 200);
+        // Both " and / must be escaped!
         $("#BGM").remove();
+        $(Music_Button).slideToggle(200);
+    }
+
+    $(Music_Button).click(function(){
+        if (status == 0) {
+            Start();
+        } else {
+            Stop();
+        }
     });
 });
