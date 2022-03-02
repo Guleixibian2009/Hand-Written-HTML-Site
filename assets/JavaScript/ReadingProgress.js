a7c95b92b68e3df301ea9a7e3223d4d9ae05f1cd
guleixibian <guleixibian@outlook.com>
2022-03-02 08:09:58 +0800

    Fix a bug on Scroll_Bar

--- assets/JavaScript/ReadingProgress.js
+++ assets/JavaScript/ReadingProgress.js
@@ -3,17 +3,23 @@ $(document).ready(function(){
     //What we need: WholeBodyHeight, CurrentScrollY
     //Then the percentage is: (CurrentScrollY + CurrentWindowHeight) / WholeBodyHeight * 100 (%)
     //There must be "+ CurrentWindowHeight" or it won't get to 100%
-    function OnScroll(){
+    function GetWidth(){
         var BodyHeight = $(document).height();
+        var CurrentWindowHeight = $(window).height();
         //Modern browser || Safari || IE
         var ScrollY = document.documentElement.scrollTop || window.pageYOffset || document.body.scrollTop;
-        var percentage = (ScrollY / BodyHeight) * 100;
+        var percentage = ((ScrollY + CurrentWindowHeight) / BodyHeight) * 100;
         const width = percentage.toFixed(2);
         return width;
     }
 
     $(window).scroll(function(){
-        var width = OnScroll();
+        var width = GetWidth();
+        $("#Progress_Bar").css("width",`${width}%`);
+    });
+
+    $(window).resize(function(){
+        var width = GetWidth();
         $("#Progress_Bar").css("width",`${width}%`);
     });
 });
