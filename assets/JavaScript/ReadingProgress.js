8758efa67ca3dc67c5f889e61a95eb21bb47ad6e
guleixibian <guleixibian@outlook.com>
2022-03-02 21:20:51 +0800

    Fix a bug of Progress_Bar

--- assets/JavaScript/ReadingProgress.js
+++ assets/JavaScript/ReadingProgress.js
@@ -1,25 +1,31 @@
 "use strict";
 $(document).ready(function(){
-    //What we need: WholeBodyHeight, CurrentScrollY
-    //Then the percentage is: (CurrentScrollY + CurrentWindowHeight) / WholeBodyHeight * 100 (%)
-    //There must be "+ CurrentWindowHeight" or it won't get to 100%
-    function GetWidth(){
+    //What we need: WholeBodyHeight, CurrentScrollY, CurrentWindowHeight
+    //Then the percentage is: CurrentScrollY / (WholeBodyHeight - CurrentWindowHeight) * 100 (%)
+    //There must be "- CurrentWindowHeight" or it won't get to 100%
+    function GetPercentage(){
         var BodyHeight = $(document).height();
         var CurrentWindowHeight = $(window).height();
         //Modern browser || Safari || IE
         var ScrollY = document.documentElement.scrollTop || window.pageYOffset || document.body.scrollTop;
         var percentage = (ScrollY / (BodyHeight - CurrentWindowHeight)) * 100;
+        return percentage;
+    }
+
+    function GetBannerWidth(){
+        const percentage = GetPercentage();
         const width = percentage.toFixed(2);
         return width;
     }
 
     $(window).scroll(function(){
-        var width = GetWidth();
+        //Get Progress_Bar width
+        var width = GetBannerWidth();
         $("#Progress_Bar").css("width",`${width}%`);
     });
 
     $(window).resize(function(){
-        var width = GetWidth();
+        var width = GetBannerWidth();
         $("#Progress_Bar").css("width",`${width}%`);
     });
 });
