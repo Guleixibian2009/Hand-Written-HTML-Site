0377e5f3e4092f8f9fce021f78db79000cc59836
guleixibian <guleixibian@outlook.com>
2022-03-02 14:51:50 +0800

    Finally fix the bug of Copy_Button

--- assets/JavaScript/CopyCodeblock.js
+++ assets/JavaScript/CopyCodeblock.js
@@ -1,25 +1,30 @@
 "use strict";
 $(document).ready(function(){
     $(".Copy_Button").click(function(){
-        var clip = new ClipboardJS(".Copy_Button",{
-            target:(trigger) => trigger.previousElementSibling,
-        });
+        const CodeBlock = $(".Copy_Button").prev("span");
+        var CopyCode = CodeBlock.text();
 
-        clip.on('success',(e)=>{
+        const Clip = new ClipboardJS(".Copy_Button",{
+            text : function(){
+                return CopyCode;
+            }
+        });
+        
+        Clip.on('success',(e)=>{
             console.log("Successfully copied to clipboard!");
             e.clearSelection();
-            clip.destroy();
+            Clip.destroy();
+
+            const Button = $(".Copy_Button");
+            Button.html("<i class=\"fa fa-check fa-fw\"></i>");
+            window.setTimeout(function(){
+                Button.html("<i class=\"fa fa-clone fa-fw\"></i>");
+            },1000);
         });
           
-        clip.on('error',()=>{
+        Clip.on('error',()=>{
             console.error("Copy Failed!");
-            clip.destroy();
+            Clip.destroy();
         });
-
-        const Button = $(".Copy_Button");
-        Button.html("<i class=\"fa fa-check fa-fw\"></i>");
-        window.setTimeout(function(){
-            Button.html("<i class=\"fa fa-clone fa-fw\"></i>");
-        },1000);
     });
 });
