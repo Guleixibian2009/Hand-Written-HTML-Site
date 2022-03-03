bcad4349a6dd804354d6932bbbf7d2b52d079aec
guleixibian <guleixibian@outlook.com>
2022-03-03 16:03:06 +0800

    Optimize the js of Music_Button

--- assets/JavaScript/BackgroundMusic.js
+++ assets/JavaScript/BackgroundMusic.js
@@ -3,13 +3,13 @@ $(document).ready(function(){
     const Music_Button = $("#Music");
     var status = 0;
     // Not Playing = 0; Playing = 1;
-    document.getElementById("Music").innerHTML = " <i class=\"fas fa-play\"></i> Music!";
+    Music_Button.html(" <i class=\"fas fa-play\"></i> Music!");
 
     function Start(){
         status = 1;
         $(Music_Button).slideToggle(200);
         window.setTimeout(function(){
-            document.getElementById("Music").innerHTML = "\<i class=\"fas fa-pause\"\>\</i\> Axero/Itro--Move";
+            Music_Button.html(" <i class=\"fas fa-pause\"></i> Axero/Itro--Move");
         }, 200);
         // Both " and / must be escaped!
         $(Music_Button).after(" <audio id=\"BGM\" autoplay=\"autoplay\" src=\"..\/Hand-Written-HTML-Site\/assets\/Musics\/Move.mp3\"></audio>");
@@ -20,7 +20,7 @@ $(document).ready(function(){
         status = 0;
         $(Music_Button).slideToggle(200);
         window.setTimeout(function(){
-            document.getElementById("Music").innerHTML = " <i class=\"fas fa-play\"></i> Music!";
+            Music_Button.html(" <i class=\"fas fa-play\"></i> Music!");
         }, 200);
         // Both " and / must be escaped!
         $("#BGM").remove();
