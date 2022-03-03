c4d4beee1c2358ee01f7fb14db93301a65f40e43
guleixibian <guleixibian@outlook.com>
2022-03-03 11:31:03 +0800

    Fix a bug on Author Fixing

--- assets/JavaScript/ReadingProgress.js
+++ assets/JavaScript/ReadingProgress.js
@@ -1,5 +1,9 @@
 "use strict";
 $(document).ready(function(){
+    function GetScrollY() {
+        return document.documentElement.scrollTop || window.pageYOffset || document.body.scrollTop;
+    }
+
     //What we need: WholeBodyHeight, CurrentScrollY, CurrentWindowHeight
     //Then the percentage is: CurrentScrollY / (WholeBodyHeight - CurrentWindowHeight) * 100 (%)
     //There must be "- CurrentWindowHeight" or it won't get to 100%
@@ -7,7 +11,7 @@ $(document).ready(function(){
         var BodyHeight = $(document).height();
         var CurrentWindowHeight = $(window).height();
         //Modern browser || Safari || IE
-        var ScrollY = document.documentElement.scrollTop || window.pageYOffset || document.body.scrollTop;
+        var ScrollY = GetScrollY();
         var percentage = (ScrollY / (BodyHeight - CurrentWindowHeight)) * 100;
         return percentage;
     }
@@ -24,8 +28,21 @@ $(document).ready(function(){
         $("#Progress_Bar").css("width",`${width}%`);
 
         //Fix the Author div's position
-        var percentage = GetPercentage();
-        if (percentage > 53) {
+        //We also need the percentage of Header + margin-top
+        //Then it will be ${ScrollY} / ${HeaderHeight}
+        function GetHeaderHeight(){
+            var HeaderHeight = $(".Header").outerHeight();
+            var GridGap = parseInt($(".Wrapper").css("gap"));
+            var MarginTop = parseInt($("body").css("margin-top"));
+            var PaddingTop = parseInt($("body").css("padding-top"));
+            var WholeHeight = HeaderHeight + MarginTop + PaddingTop + GridGap;
+            var HeaderHeight = WholeHeight.toFixed(2);
+            return HeaderHeight;
+        }
+
+        var HeaderHeight = GetHeaderHeight();
+        var ScrollY = GetScrollY();
+        if (ScrollY > HeaderHeight) {
             $(".Author").addClass("Author_Fixed");
         } else {
             $(".Author").removeClass("Author_Fixed");
@@ -37,3 +54,4 @@ $(document).ready(function(){
         $("#Progress_Bar").css("width",`${width}%`);
     });
 });
+
