59ed1e7b6c382abbd19ad1490682f40a2d439771
guleixibian <guleixibian@outlook.com>
2022-03-04 08:17:23 +0800

    Optimize the appearance of the buttons

--- assets/JavaScript/WindowSize.js
+++ assets/JavaScript/WindowSize.js
@@ -1,19 +1,15 @@
 "use strict";
 $(document).ready(function(){
     //Horizontal = 1, Vertical = 0
-    var PageStatus = 1;
     function ShowOrHide(WindowWidth){
         if (WindowWidth <= 1000) {
             Update_Data.show(200);
-            console.debug("Showed!");
         } else {
             Update_Data.hide(200);
-            console.debug("Hided!");
         }
     }
 
     const Update_Data = $(".Main_Body_Update_Data");
-    console.debug(Update_Data);
     //Check the Width on opening
     var CurrentWindowWidth = parseInt($(window).width());
     ShowOrHide(CurrentWindowWidth);
