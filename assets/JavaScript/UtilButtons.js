931973f8d08fa3359ffd88e1bc7116f8ca6baad0
guleixibian <guleixibian@outlook.com>
2022-03-08 08:46:59 +0800

    To show the Util_Buttons when hover

---
+++ assets/JavaScript/UtilButtons.js
@@ -0,0 +1,37 @@
+//The animation for the buttons
+$(document).ready(function(){
+    const ButtonGroup = $(".Util_Buttons");
+    const BackToTop = $("#Back_To_Top");
+    const FullScreen = $("#Full_Screen");
+    const PageZoom = $("#Page_Zoom");
+    const UtilsMain = $("#Utils_Main");
+
+    function OnStart(){
+        $(BackToTop).hide();
+        $(FullScreen).hide();
+        $(PageZoom).hide();
+        $(UtilsMain).show();
+    }
+
+    function OnMainHover(){
+        $(BackToTop).show(600);
+        $(FullScreen).show(400);
+        $(PageZoom).show(200);
+    }
+
+    function MainNotHover(){
+        $(BackToTop).hide(200);
+        $(FullScreen).hide(400);
+        $(PageZoom).hide(600);
+    }
+
+    OnStart();
+
+    $(ButtonGroup).mouseenter(function(){
+        OnMainHover();
+    });
+
+    $(ButtonGroup).mouseleave(function(){
+        MainNotHover();
+    });
+});
