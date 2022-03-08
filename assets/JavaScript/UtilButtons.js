d6e97b2ae15a1886c8cb79f7517c738ad405a57d
guleixibian <guleixibian@outlook.com>
2022-03-08 10:51:18 +0800

    Enable the FullScreen Button

--- assets/JavaScript/UtilButtons.js
+++ assets/JavaScript/UtilButtons.js
@@ -45,3 +45,45 @@ $(document).ready(function(){
         $.scrollTo(0,1000);
     });
 });
+
+//The Full Screen Button
+$(document).ready(function(){
+    const FullScreen = $("#Full_Screen");
+    var ScreenStatus = 0;
+
+    function StartFullScreen(element){
+        ScreenStatus = 1;
+        if(element.requestFullscreen) {
+            element.requestFullscreen();
+        } else if(element.mozRequestFullScreen) {
+            element.mozRequestFullScreen();
+        } else if(element.webkitRequestFullscreen) {
+            element.webkitRequestFullscreen();
+        } else if(element.msRequestFullscreen) {
+            element.msRequestFullscreen();
+        } else {
+            window.alert("Sorry, your browser doesn't support RequestFullScreen.\nPlease press F11 manually.");
+        }
+    }
+
+    function exitFullscreen() {
+        ScreenStatus = 0;
+        if(document.exitFullscreen) {
+            document.exitFullscreen();
+        } else if(document.mozCancelFullScreen) {
+            document.mozCancelFullScreen();
+        } else if(document.webkitExitFullscreen) {
+            document.webkitExitFullscreen();
+        } else {
+            window.alert("Sorry, your browser doesn't support ExitFullScreen.\nPlease press F11 or ESC manually.");
+        }
+    }
+
+    $(FullScreen).click(function(){
+        if (ScreenStatus == 0){
+            StartFullScreen(document.documentElement);
+        } else {
+            exitFullscreen();
+        }
+    });
+});
